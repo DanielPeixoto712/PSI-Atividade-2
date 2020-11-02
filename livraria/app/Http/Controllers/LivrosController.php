8798db8ffca12c04efa25b63c83d15cda4d93990
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-11-02 11:28:21 +0000

    Update .editorconfig, .env.example, and 87 more files...

---
+++ livraria/app/Http/Controllers/LivrosController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Livro;
+
+class LivrosController extends Controller
+{
+   public function index(){
+   	$livros = Livro::all();
+
+   	return view('livros.index', ['livros'=>$livros
+   ]);
+   }
+}
